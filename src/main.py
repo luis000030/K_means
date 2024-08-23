288596f7401c435c1d065e9e1dcdd60bde8aa1de
luis000030 <luis.garfez@gmail.com>
2024-08-23 14:59:13 +0200

    Rutas relativas

--- src/main.py
+++ src/main.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 from algoritmo.k_means import*
 from PIL import Image
 
-imagen = Image.open('D:\\Programacion\\Python\\ML\\K-means\\data\\Atardecer.jpeg')
+imagen = Image.open('data\\Atardecer_colorido.jpeg')
 
 array_imagen = np.array(imagen)
 WIDTH = array_imagen.shape[0]
 HEIGHT = array_imagen.shape[1]
 imagen_reordenada = array_imagen.reshape(-1, 3)
 
-num_centroides = 64
+num_centroides = 128
 
-centroides = correr_algoritmo_varias_veces(imagen_reordenada, k=num_centroides, num_iters=16, umbral=0.001, num_veces=16, show=True)
+centroides = correr_algoritmo_varias_veces(imagen_reordenada, k=num_centroides, num_iters=16, umbral=0.001, num_veces=32, show=True)
 centroides_cercanos = calcular_centroides_cercanos(imagen_reordenada, centroides)
 for j in range(num_centroides):
     mask_j = (centroides_cercanos == j)
@@ -21,4 +21,4 @@ for j in range(num_centroides):
 array_imagen_comprimida = imagen_reordenada.reshape(WIDTH, HEIGHT, -1)
 array_imagen_comprimida = array_imagen_comprimida.astype(np.uint8)
 imagen_comprimida = Image.fromarray(array_imagen_comprimida)
-imagen_comprimida.save('D:\\Programacion\\Python\\ML\\K-means\\data\\Atardecer_comprimido_524.jpeg')
+imagen_comprimida.save('data\\Imagen_comprimida_128_1.jpeg')
